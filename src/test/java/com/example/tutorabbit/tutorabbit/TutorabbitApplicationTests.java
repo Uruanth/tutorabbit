99dbdebba29e928d67950fe0bfa683d8a7b6001f
DaironPerilla <perrynikon@gmail.com>
2022-02-21 10:51:13 -0500

    Initial commit

---
+++ src/test/java/com/example/tutorabbit/tutorabbit/TutorabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorabbit.tutorabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
